75092d58f7da0d33aeea08e33bcdbacb4dfa6b11
Stephan Schubert <stephan@frozencherry.de>
2012-02-11 12:48:44 +0100

    Bugfix: We want instance methods.

--- lib/mongoid/seo/aliases.rb
+++ lib/mongoid/seo/aliases.rb
@@ -12,7 +12,7 @@ module Mongoid
             field "#{name}_aliases".to_sym, type: Array, default: []
             before_save "update_#{name}_aliases".to_sym, :if => "#{name}_changed?".to_sym
 
-            instance_eval <<-CODE
+            class_eval <<-CODE
               def update_#{name}_aliases
                 #{name}_aliases.push(#{name}_was)
               end
